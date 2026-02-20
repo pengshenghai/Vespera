bfabc236e2956c7c47c78920b8f35054469b0aaf
ayo-ola0710 <israelolatunle2015@gmail.com>
2026-02-20 22:40:19 +0100

    added the styling to the pages created

--- frontend/app/privacy/page.tsx
+++ frontend/app/privacy/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const page = () => {
-  return <div>These is the privacy policies page</div>;
+  return (
+    <div className="min-h-screen bg-brand-gradient selection:bg-white/30 selection:text-white text-4xl text-white">
+      These is the privacy policies page
+    </div>
+  );
 };
 
 export default page;
--- frontend/app/terms/page.tsx
+++ frontend/app/terms/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const page = () => {
-  return <div>These is the terms and condition page</div>;
+  return (
+    <div className="min-h-screen bg-brand-gradient selection:bg-white/30 selection:text-white text-4xl text-white">
+      These is the terms and condition page
+    </div>
+  );
 };
 
 export default page;
