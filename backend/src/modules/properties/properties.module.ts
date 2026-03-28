569477e4fdc96ff2a4f7d50ccfacbf1642871793
Bamford <bamfordjnr5@gmail.com>
2026-03-28 02:34:08 +0100

    fix: resolve merge conflict in properties.module.ts

--- backend/src/modules/properties/properties.module.ts
+++ backend/src/modules/properties/properties.module.ts
@@ -2,16 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PropertiesController } from './properties.controller';
+import { PropertyWizardController } from './property-wizard.controller';
 import { PropertiesService } from './properties.service';
+import { PropertyWizardService } from './property-wizard.service';
 import { PropertyCacheWarmingService } from './property-cache-warming.service';
 import { PropertyModesService } from './services/property-modes.service';
 import { PropertyModesController } from './controllers/property-modes.controller';
+import { CacheService } from '../../common/cache/cache.service';
 import { Property } from './entities/property.entity';
 import { PropertyImage } from './entities/property-image.entity';
 import { PropertyAmenity } from './entities/property-amenity.entity';
 import { RentalUnit } from './entities/rental-unit.entity';
 import { PropertyListingDraft } from './entities/property-listing-draft.entity';
-import { CacheService } from '../../common/cache/cache.service';
 
 @Module({
   imports: [
@@ -24,13 +26,18 @@ import { CacheService } from '../../common/cache/cache.service';
       PropertyListingDraft,
     ]),
   ],
-  controllers: [PropertiesController, PropertyModesController],
+  controllers: [
+    PropertiesController,
+    PropertyWizardController,
+    PropertyModesController,
+  ],
   providers: [
     PropertiesService,
+    PropertyWizardService,
     PropertyCacheWarmingService,
     CacheService,
     PropertyModesService,
   ],
-  exports: [PropertiesService, PropertyModesService],
+  exports: [PropertiesService, PropertyWizardService, PropertyModesService],
 })
 export class PropertiesModule {}
